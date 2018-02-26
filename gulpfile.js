b4f052b7c745437ce06cb82c995ceeaea3da8222
ciolt <cioltdev@outlook.com>
2018-02-25 21:40:20 -0500

    add data json

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
+var jsonminify = require('gulp-jsonminify');
 
 gulp.task('html', function () {
   return gulp.src(['./views/**/*.pug', '!./views/**/_*.pug'])
@@ -20,9 +21,17 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./dist/css'));
 });
 
+gulp.task('data', function () {
+  return gulp.src(['./_data/**/*.*'])
+    .pipe(gulp.dest(function (file) {
+      return file.base.split('_data\\')[0] + 'dist\\data\\' + file.base.split('_data\\')[1];
+    }))
+});
+
 gulp.task('watch', function(){
   gulp.watch('./css/**/*.scss', ['sass']);
-  gulp.watch('./views/**/*.pug', ['html']); 
+  gulp.watch('./views/**/*.pug', ['html']);
+  gulp.watch('./_data/**/*.*', ['data']);
 })
 
 gulp.task('default', ['html', 'sass']);
